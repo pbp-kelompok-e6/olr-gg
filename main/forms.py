da38da39156298499c7de6cf8e0f67aff17081b9
milnyet <jackboiii4343@gmail.com>
2025-10-23 00:14:15 +0700

    perbaikan model yang ada di main konflik dengan model berita

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from main.models import News
-
-class NewsForm(ModelForm):
-    class Meta:
-        model = News
-       
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from main.models import News
+from berita.models import News
 from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.contrib import messages
