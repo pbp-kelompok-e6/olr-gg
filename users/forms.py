3c46c8012a7b3702cfddfdf46a0cd10a2bb527e7
matthewnathanael <matthewnathanael04@gmail.com>
2025-10-24 16:28:57 +0700

    No changes again, need to commit before pulling

--- users/forms.py
+++ users/forms.py
@@ -1,28 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser, Report
+from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('first_name', 'last_name', 'username',)
+        fields = ('username',)
 
 class CustomUserChangeForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'bio',]  
-    
-class ProfilePictureForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['profile_picture',]
-
-class ReportUserForm(forms.ModelForm):
-    reason = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 4, 'placeholder': 'Jelaskan alasan Anda...'}),
-        label="Alasan Laporan"
-    )
-    
-    class Meta:
-        model = Report
-        fields = ['reason']
+        fields = ['bio', 'profile_picture']  
